dcb32b270889ce32a23eba2808c265ec3c36b20f
jimmyforsstrom <jimmyforsstrom>
2021-09-23 11:42:54 +0200

    Lade till en while loop

--- Main.java
+++ Main.java
@@ -9,22 +9,36 @@ public class Main {
         int summa = 0;
         Room rum1 = new Room("Jupiter", 3000);
         Room rum2 = new Room("Mars", 1600);
+        Room rum3 = new Room("Tellus", 1600);
+        Room rum4 = new Room("Pluto", 1600);
+        Room rum5 = new Room("Saturnus", 3000);
+        Room rum6 = new Room("Venus", 1600);
+
 
         List<String> rumslista = new ArrayList();
-        rumslista.add(rum1.rumsnamn + " med en kostnad på " +  rum1.Rumskostnad);
+        rumslista.add("Rum 1 " + rum1.rumsnamn +  "- Pris per natt " +  rum1.Rumskostnad);
+        rumslista.add("Rum 2 " + rum2.rumsnamn + "- Pris per natt  " +  rum2.Rumskostnad);
+        rumslista.add("Rum 3 " + rum3.rumsnamn + "- Pris per natt  " +  rum3.Rumskostnad);
+        rumslista.add("Rum 4 " + rum4.rumsnamn + "- Pris per natt  " +  rum4.Rumskostnad);
+        rumslista.add("Rum 5 " + rum5.rumsnamn + "- Pris per natt  " +  rum5.Rumskostnad);
+        rumslista.add("Rum 6 " + rum6.rumsnamn + "- Pris per natt  " +  rum6.Rumskostnad);
+
     Startsida.menu();
     int val = scanner.nextInt();
 
     switch (val) {
         case 1:
-            System.out.println(rumslista);
-
         break;
         case 2:
             break;
         case 3:
+            for (String s: rumslista
+            ) {
+                System.out.println(s);
+            }
             break;
         case 4:
+
             break;
         case 5:
             summa = rum1.Rumskostnad;
