1089f048579dd838d2a9f675d8a27922df8adc1a
jimmyforsstrom <jimmyforsstrom>
2021-09-28 21:46:25 +0200

    Snyggade till Salmetod och lade till en whileloop

--- Main.java
+++ Main.java
@@ -9,40 +9,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean rullarMenyn = true;
+        int val = 0;
+        Sal[] mittHotell = new Sal[3];
+        mittHotell[0] = new Sal("Jupiter", 2000);
+        mittHotell[1] = new Sal("Mars", 1500);
+        mittHotell[2] = new Sal("Venus", 1250);
 
-        Sal [] mittHotell = new Sal[3];
-        mittHotell [0] = new Sal("Jupiter", 2000);
-        mittHotell [1] = new Sal("Mars", 1500);
-        mittHotell [2] = new Sal("Venus", 1250);
-
-        Sal [] bokadeRum = new Sal[3];
-        bokadeRum [0] = new Sal("", 0);
-        bokadeRum [1] = new Sal("", 0);
-        bokadeRum [2] = new Sal("", 0);
+        Sal[] bokadeRum = new Sal[3];
+        bokadeRum[0] = new Sal("", 0);
+        bokadeRum[1] = new Sal("", 0);
+        bokadeRum[2] = new Sal("", 0);
 
         Startsida.menu();
-        int val = 0;
-        val = scanner.nextInt();
-        switch (val) {
-            case 1:
-                Salmetod.BokaSal(mittHotell, bokadeRum, val);
-                break;
-            case 2:
-                Salmetod.BokadeSalar(mittHotell, bokadeRum, val);
-                break;
-            case 3: Salmetod.seSalar(mittHotell, bokadeRum);
-        }
 
+        do {
+            val = scanner.nextInt();
 
-
-
-
-
-
-
-
+            switch (val) {
+                case 1:
+                    Salmetod.BokaSal(mittHotell, bokadeRum, val);
+                    break;
+                case 2:
+                    Salmetod.BokadeSalar(mittHotell, bokadeRum, val);
+                    break;
+                case 3:
+                    Salmetod.seSalar(mittHotell, bokadeRum);
+            }
+        }  while (val != 6) ;
     }
-
-
 }
+
 
--- Salmetod.java
+++ Salmetod.java
@@ -4,29 +4,45 @@ import java.util.Scanner;
 
 public class Salmetod {
 
-
-    public static void seSalar(Sal[]mittHotell, Sal[]bokadeRum) {
-        for (Sal s: mittHotell
-        ) {
-            System.out.println(s.getNamn());
-        }
-    }
-    public static void BokaSal (Sal[]mittHotell, Sal[]bokadeRum, int val) {
+    //Meny 1
+    public static void BokaSal(Sal[] mittHotell, Sal[] bokadeRum, int val) {
         System.out.println("<-------------Please pick one-------------->");
         Salmetod.seSalar(mittHotell, bokadeRum);
         Scanner scanner = new Scanner(System.in);
-        val = scanner.nextInt() -1;
-        System.out.println("Du valde " +mittHotell[val].getNamn()+ " till en kostnad för " + mittHotell[val].getCost());
+        val = scanner.nextInt() - 1;
+        System.out.println("Du valde " + mittHotell[val].getNamn() + " till en kostnad för " + mittHotell[val].getCost());
+        bokadeRum[val] = mittHotell[val];
+
+        scanner.nextLine();
+        Startsida.menu();
     }
 
+    //Meny 2
     public static void BokadeSalar(Sal[] mittHotell, Sal[] bokadeRum, int val) {
-        System.out.println("vilket rum vill du boka ut ifrån? ");
+        System.out.println("vilket rum vill du checka ut ifrån? ");
         Salmetod.seSalar(mittHotell, bokadeRum);
         Scanner scanner = new Scanner(System.in);
-        val = scanner.nextInt()-1;
-        System.out.println("Du valde att boka ut från "+ mittHotell[val].getNamn());
-        System.out.println("Din räkning för rummet är " + mittHotell[val].getCost());
-        //Tar bort indexet från bokaRum, samt addera det mitthotell?
+        val = scanner.nextInt() - 1;
+        System.out.println("Du valde att boka ut från " + bokadeRum[val].getNamn()); //Ändrat till bokade rum.namn.
+        System.out.println("Din räkning för rummet är " + bokadeRum[val].getCost()); //Ändrat till bokade rum kostnad
+
+        System.out.println("<------- Nu är rummet ledigt för andra att boka --------->");
+
+        scanner.nextLine();
+        Startsida.menu();
+        //Tar bort indexet från bokadeRum, samt addera det mitthotell?
+    }
+
+    //Meny 3
+    public static void seSalar(Sal[] mittHotell, Sal[] bokadeRum) {
+        for (Sal s : mittHotell
+        ) {
+            System.out.println(s.getNamn() + " och kostar " + s.getCost());
+        }
+        System.out.println("-----------------------");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        Startsida.menu();
     }
 }
 
